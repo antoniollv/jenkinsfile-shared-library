c1f61b65acdcff7cba288ad7d3ad37235a6d81b6
AntonioLLV <112643224+antoniollv@users.noreply.github.com>
2023-06-08 08:28:26 +0200

    Update showEnvironment.groovy
    Update showEnvironment.groovy

--- vars/showEnvironment.groovy
+++ vars/showEnvironment.groovy
@@ -1,1 +1,17 @@
-
+def call() {
+    echo "Running Job: ${env.JOB_NAME} Build: ${env.BUILD_ID} on ${env.JENKINS_URL}"
+    echo "----------- HOSTNAME --------------"
+    sh "hostname"
+    echo "-----------------------------------"
+    echo "-------- GLOBALS VARIABLES --------"
+    echo "PATH = ${env.PATH}"
+    echo "WORKSPACE= ${env.WORKSPACE}"
+    echo "-----------------------------------"
+    echo "---- ALL ENVIRONMENT VARIABLES ----"
+    sh 'printenv'
+    echo "-----------------------------------"
+    echo "-------- ALL POM VARIABLES --------"
+    echo "-----------------------------------"
+    echo "----- ALL BITBUCKET VARIABLES -----"
+    echo "-----------------------------------"
+}

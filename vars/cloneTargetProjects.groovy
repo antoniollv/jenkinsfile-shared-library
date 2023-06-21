606088842de4a95a3687700c91e344dcdd32d206
Antonio Lledó Valero <antonio.lledo.valero@accenture.com>
2023-06-21 13:28:10 +0200

    Fix error bracket

--- vars/cloneTargetProjects.groovy
+++ vars/cloneTargetProjects.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
 def call (REPOSITORIOS) {
   sh """IFS="," read -a repos <<< ${REPOSITORIOS}
-for repo in "${repos}[@]}"
+for repo in "\${repos}[@]}"
   do
-    echo $repo
+    echo \$repo
 done
 """
 }

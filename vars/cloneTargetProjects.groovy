39a8f5082556370f6ecd4df56cbceb4c2c1de5a1
Antonio Lledó Valero <antonio.lledo.valero@accenture.com>
2023-06-21 14:01:24 +0200

    Arrays from groovy

--- vars/cloneTargetProjects.groovy
+++ vars/cloneTargetProjects.groovy
@@ -1,9 +1,8 @@
 #!/usr/bin/env groovy
 def call (REPOSITORIOS) {
-  sh """repos=(${REPOSITORIOS})
-for repo in "\${repos}[@]}"
-  do
-    echo \$repo
-done
-"""
+    String[] repos
+    repos = repos.split(',')
+    for(String repo : repos) {
+        println(repo)
+    }
 }

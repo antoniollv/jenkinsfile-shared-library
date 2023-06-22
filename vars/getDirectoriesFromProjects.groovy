7e58a4a5c23a731a801a29560e50e458f47da4e0
Antonio Lledó Valero <antonio.lledo.valero@accenture.com>
2023-06-22 15:13:18 +0200

    Mode debug off

--- vars/getDirectoriesFromProjects.groovy
+++ vars/getDirectoriesFromProjects.groovy
@@ -6,10 +6,8 @@ def call(String projects) {
     String[] reposTmp
     reposTmp = projects.split(',')
     for(int i = 0; i < reposTmp.size(); ++i) {
-        println(reposTmp[i])
         directory = getDirectoryFromProject(reposTmp[i])
         repos.add(directory)
-        println(repos[i])
     }
     return repos
 }

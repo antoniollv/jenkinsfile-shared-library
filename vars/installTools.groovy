a10270867b2188ee8c4c4f06f6f6c7145604fd47
Antonio Lledó Valero <antonio.lledo.valero@accenture.com>
2023-06-22 09:49:36 +0200

    Agent doker image bitnami/git

--- vars/installTools.groovy
+++ vars/installTools.groovy
@@ -1,9 +1,14 @@
 #!/usr/bin/env groovy
 def call(env){
+//     sh '''
+// apt-get update
+// git  --version || apt-get install -y git
+// diff --version || apt-get install -y diff
+// rm -rf /var/lib/apt/lists/* 
+// '''
     sh '''
-apt-get update
 git  --version || apt-get install -y git
-diff --version || apt-get install -y diff
+diff --version || echo "¡ATENCION! diff no esta Instalado"
 rm -rf /var/lib/apt/lists/* 
-'''    
+'''
 }

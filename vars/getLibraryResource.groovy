d95da7dc5e6bd2001e833f89daf3306507104b0f
Antonio Lledó Valero <antonio.lledo.valero@accenture.com>
2023-09-04 08:53:53 +0200

    Add funtion getLibraryResource

---
+++ vars/getLibraryResource.groovy
@@ -0,0 +1,10 @@
+/**
+*  Método para obtener un recurso desde librería como archivo en el sistema de ficheros del nodo.
+*
+*  @param resource    Recuros en la librería
+*  @param nodePath    Path donde se creara el recurso como archivo
+*/
+def call(resource, nodePath) {
+    def resourceFile = libraryResource "com/moradores/${resource}"
+    sh(returnStatus: false, script: "echo \"$resorceFile\" > \"$nodePath\"")
+}

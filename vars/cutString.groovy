add553d8f104676d84bb8269e925ef2dc6b74af4
Antonio Lledó Valero <antonio.lledo.valero@accenture.com>
2023-09-27 09:10:36 +0200

    Add cut string method

---
+++ vars/cutString.groovy
@@ -0,0 +1,10 @@
+
+def call(String string, String cutChar, boolean right = false) {
+    def posCutChar = string.indexOf(cutChar)
+    if (right) {
+        string  = string.substring(posCutChar + 1)
+    } else {
+        string  = string.getAt(0..posCutChar)
+    }
+    return string
+}

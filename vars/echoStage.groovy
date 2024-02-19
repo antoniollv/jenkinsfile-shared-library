d13a58e7a275ff17ab443d3669a5dfa9a481a456
Antonio Lledo <alledova@gmail.com>
2024-02-19 19:53:45 +0100

    nivelación

--- vars/echoStage.groovy
+++ vars/echoStage.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 def call() {
     def guiones = 50
-    def pad     = 20
+    //def pad     = 20
     println ""
     println "".center(guiones,"-")
     println " ${STAGE_NAME} ".center(guiones,"-")

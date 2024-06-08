ec18f85b36755168aa4295a7763a01496d5ab8c9
AntonioLLV <112643224+antoniollv@users.noreply.github.com>
2024-06-08 16:39:10 +0200

    Update loadAndShowYaml.groovy

--- vars/loadAndShowYaml.groovy
+++ vars/loadAndShowYaml.groovy
@@ -3,11 +3,11 @@ import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.DumperOptions
 import static org.yaml.snakeyaml.DumperOptions.FlowStyle.BLOCK
 
-def call() {
-    getLibraryResource('mapfre/tron-pipeline-release.ORIGEN.yml', 'tron-pipeline-release.ORIGEN.yml')
-    def dataYaml = readYaml  file: 'tron-pipeline-release.ORIGEN.yml'
+def call(String ruta, String yamlName, String pais) {
+    getLibraryResource("${ruta}/${yamlName}", yamlName)
+    def dataYaml = readYaml  file: yamlName
     def projectConfig = dataYaml.project_config
-    def traductionPais = new traductionLanzador().getTraductions('Panama')
+    def traductionPais = new traductionLanzador().getTraductions(pais)
     def projects = []
     projectConfig.each { project ->        
         projects.add(loadMap(project, traductionPais))
@@ -33,21 +33,23 @@ def loadMap(project, traductionPais) {
 }
 
 class traductionLanzador {
-    private Map traductionsPanama = [  
-        'core_tron_be_nwt': 'ca_tron_be_nwt',
-        'core_tron_be_nwt': 'ca_tron_be_nwt',
-        'core_tron_cmn_api': 'ca_tron_cmn_api',
-        'core_tron_fw_tw': 'ca_tron_fw_tw',
-        'core_tron_client_tw': 'ca_tron_client_tw',
-        'core_tron_client_tst_tw': 'ca_tron_client_tst_tw',
-        'core_tron_bom_tw': 'ca_tron_bom_tw',
-        'core_tron_btc_api': 'ca_tron_btc_api'                      
+    private Map traductionsPais01 = [  
+        'clave0': 'valorClave0Pais01',
+        'clave1': 'valorClave1Pais01'                      
+    ]
+
+    private Map traductionsPais02 = [  
+        'clave0': 'valorClave0Pais02',
+        'clave1': 'valorClave1Pais02'                      
     ]
 
     def getTraductions( String pais) {
         switch (pais) {
-        case 'Panama':
-            return traductionsPanama
+        case 'Pais01':
+            return traductionsPais01
+            break
+        case 'Pais02':
+            return traductionsPais02
             break
         }
     }

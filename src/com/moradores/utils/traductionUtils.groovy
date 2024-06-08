3d330f104e7570342e02737ca1ff43330e1ff5b3
AntonioLLV <112643224+antoniollv@users.noreply.github.com>
2024-06-08 16:42:20 +0200

    Update traductionUtils.groovy

--- src/com/moradores/utils/traductionUtils.groovy
+++ src/com/moradores/utils/traductionUtils.groovy
@@ -4,13 +4,8 @@ class traductionUtils {
 
     private Map traductions = [  
         //dependencias de core
-        PROJECT_BACKEND_VERSION: 'core_tron_be_nwt',
-        NEWTRON_BE_VERSION: 'core_tron_be_nwt',
-        NWT_CMN_API_VERSION: 'core_tron_cmn_api',
-        'version.twframework': 'core_tron_fw_tw',
-        'version.twclient.trn': 'core_tron_client_tw',
-        'version.twclient.tst': 'core_tron_client_tst_tw',
-        'version.twbom': 'core_tron_bom_tw'                      
+        'clave00': valor,
+        'clave01': valor                     
     ]
 
     def getTraductions() {
